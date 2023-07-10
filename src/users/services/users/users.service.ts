ffea89f944817549b6c9d206f3e1268a92629480
phan.anh <leducphananh@gmail.com>
2023-07-10 13:37:25 +0700

    exceptions and handling exceptions

--- src/users/services/users/users.service.ts
+++ src/users/services/users/users.service.ts
@@ -5,18 +5,22 @@ import { SerializedUser, User } from 'src/users/types';
 export class UsersService {
   private users: User[] = [
     {
+      id: 1,
       username: 'danny',
       password: 'danny',
     },
     {
+      id: 2,
       username: 'anson',
       password: 'anson',
     },
     {
+      id: 3,
       username: 'derek',
       password: 'derek',
     },
     {
+      id: 4,
       username: 'samantha',
       password: 'samantha',
     },
@@ -28,5 +32,9 @@ export class UsersService {
 
   getUserByUsername(username: string) {
     return this.users.find((user) => user.username === username);
+  }
+
+  getUserById(id: number) {
+    return this.users.find((user) => user.id === id);
   }
 }
